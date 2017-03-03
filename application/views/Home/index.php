a84010567bbcfbea722848adcbe0b68a0928730a
NAROLA\vparth <pav@narola.email>
2017-03-03 09:15:36 +0530

    no message

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -1,3 +1,4 @@
+<?php if(!empty($posts)){ ?>
 <a target="_blank" href="<?php echo $posts->posts[0]->url; ?>" class="homeV1MainPost">
     <img src="<?php echo ($posts->posts[0]->thread->main_image != '') ? $posts->posts[0]->thread->main_image : 'assets/images/main_post.jpg'; ?>" alt="Francoise img">
     <div class="overlayBox">
@@ -32,4 +33,4 @@
         <li class="newPosts"><a href="#">New posts</a></li>
         <li class="olderPosts"><a href="">Older posts</a></li>
     </ul>
-</div>
+</div><?php } ?>
